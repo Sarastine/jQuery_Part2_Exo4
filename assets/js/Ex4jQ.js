d7633b3b4952a4301168504103b800dfe2722401
Caillou <Boucher.Pierre@hotmail.fr>
2018-10-15 11:55:10 +0200

    Exercice fait

---
+++ assets/js/Ex4jQ.js
@@ -0,0 +1,9 @@
+$(".red").click(function(){
+	$("#text").css('color', 'red');
+});
+$(".green").click(function(){
+	$("#text").css('color', 'green');
+});
+$(".blue").click(function(){
+	$("#text").css('color', 'blue');
+});
